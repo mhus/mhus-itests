01c1bf74c9d125917f9139902ce5af4375362eb5
Mike Hummel <mike.hummel@billingsolutions.de>
2020-07-16 10:51:24 +0200

    Update SimpleTest.java

--- test-simple/src/test/java/de/mhus/lib/itest/simple/SimpleTest.java
+++ test-simple/src/test/java/de/mhus/lib/itest/simple/SimpleTest.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import com.github.dockerjava.api.exception.DockerException;
 
@@ -16,63 +19,78 @@ import de.mhus.lib.tests.docker.DockerScenario;
 import de.mhus.lib.tests.docker.Karaf;
 import de.mhus.lib.tests.docker.LogStream;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class SimpleTest extends TestCase {
 
-	private static DockerScenario scenario;
+    private static DockerScenario scenario;
 
-	
-	@Test
-	public void testSample() throws NotFoundException, DockerException, InterruptedException, IOException {
-	    
+    
+    @Test
+    @Order(1)
+    public void testSample() throws NotFoundException, DockerException, InterruptedException, IOException {
+        
         System.out.println("STEP 1 ===========================");
-	    try {
-	        scenario.waitForLogEntry("karaf", "@karaf()>", 0);
-	    } catch (EOFException e) {
-	        e.printStackTrace();
-	    }
+        try {
+            scenario.waitForLogEntry("karaf", "@karaf()>", 0);
+        } catch (EOFException e) {
+            e.printStackTrace();
+        }
 
-	    System.out.println("STEP 2 ===========================");
-	    try (LogStream stream = scenario.attach("karaf", "echo \"Hello World\"\n" )) {
-	        scenario.waitForLogEntry(stream, "Hello World");
-	    }
-	}
-	
-	@BeforeAll
-	public static void startDocker() {
-	    
-//	    System.out.println("ENV:");
+        System.out.println("STEP 2 ===========================");
+        try (LogStream stream = scenario.attach("karaf", "echo \"Hello World\"\n" )) {
+            scenario.waitForLogEntry(stream, "Hello World");
+        }
+    }
+    
+    @Test
+    @Order(2)
+    public void testInstallFeature() throws NotFoundException, DockerException, InterruptedException, IOException {
+        
+        System.out.println("Install features");
+        try (LogStream stream = scenario.attach("karaf", 
+                "feature:repo-add mvn:org.apache.shiro/shiro-features/1.5.1/xml/features\n" + 
+                "feature:repo-add mvn:de.mhus.osgi/mhus-features/7.1.0-SNAPSHOT/xml/features\n" +
+                "feature:install mhu-base\n" )) {
+            scenario.waitForLogEntry(stream, "Hello World");
+        }
+    }
+    
+    @BeforeAll
+    public static void startDocker() {
+        
+//      System.out.println("ENV:");
 //        System.out.println(System.getenv());
 //        System.out.println("PROPS:");
 //        System.out.println(System.getProperties());
         
-		scenario = new DockerScenario();
-//		scenario.add("jms", "webcenter/activemq:5.14.3", 
-//				"env:ACTIVEMQ_CONFIG_NAME=amqp-srv1",
-//				"env:ACTIVEMQ_CONFIG_DEFAULTACCOUNT=false",
-//				"env:ACTIVEMQ_ADMIN_LOGIN=admin",
-//				"env:ACTIVEMQ_ADMIN_PASSWORD=nein",
-//				"env:ACTIVEMQ_CONFIG_MINMEMORY=1024",
-//				"env:ACTIVEMQ_CONFIG_MAXMEMORY=4096",
-//				"env:ACTIVEMQ_CONFIG_SCHEDULERENABLED=true"
-//				);
-//		
-//		scenario.add("db", "mariadb:10.3", 
-//		        "e:MYSQL_ROOT_PASSWORD=nein"
-//		        );
-		
-		scenario.add(new Karaf("karaf", null, "debug"));
-		
-		scenario.destroyPrefix();
-		scenario.start();
-		
-		MThread.sleep(1000);
-		
-	}
-	
-	@AfterAll
-	public static void stopDocker() {
-		scenario.destroy();
-	}
-	
-	
+        scenario = new DockerScenario();
+//      scenario.add("jms", "webcenter/activemq:5.14.3", 
+//              "env:ACTIVEMQ_CONFIG_NAME=amqp-srv1",
+//              "env:ACTIVEMQ_CONFIG_DEFAULTACCOUNT=false",
+//              "env:ACTIVEMQ_ADMIN_LOGIN=admin",
+//              "env:ACTIVEMQ_ADMIN_PASSWORD=nein",
+//              "env:ACTIVEMQ_CONFIG_MINMEMORY=1024",
+//              "env:ACTIVEMQ_CONFIG_MAXMEMORY=4096",
+//              "env:ACTIVEMQ_CONFIG_SCHEDULERENABLED=true"
+//              );
+//      
+//      scenario.add("db", "mariadb:10.3", 
+//              "e:MYSQL_ROOT_PASSWORD=nein"
+//              );
+        // "link:db:dbserver"
+        scenario.add(new Karaf("karaf", null, "debug"));
+        
+        scenario.destroyPrefix();
+        scenario.start();
+        
+        MThread.sleep(1000);
+        
+    }
+    
+    @AfterAll
+    public static void stopDocker() {
+        scenario.destroy();
+    }
+    
+    
 }
