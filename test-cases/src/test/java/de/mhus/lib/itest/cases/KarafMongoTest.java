54fd800cccd109d4ce6bb94b9e3a14ec016ed392
Mike Hummel <mike.hummel@billingsolutions.de>
2020-08-03 17:46:39 +0200

    start with mongo test cases

---
+++ test-cases/src/test/java/de/mhus/lib/itest/cases/KarafMongoTest.java
@@ -0,0 +1,132 @@
+package de.mhus.lib.itest.cases;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+import de.mhus.lib.core.MProperties;
+import de.mhus.lib.core.MThread;
+import de.mhus.lib.errors.NotFoundException;
+import de.mhus.lib.tests.TestCase;
+import de.mhus.lib.tests.docker.AnsiLogFilter;
+import de.mhus.lib.tests.docker.DockerScenario;
+import de.mhus.lib.tests.docker.Karaf;
+import de.mhus.lib.tests.docker.LogStream;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class KarafMongoTest extends TestCase {
+
+    private static DockerScenario scenario;
+    private static MProperties prop;
+
+    @Test
+    @Order(1)
+    public void testJmsConnection() throws NotFoundException, IOException, InterruptedException {
+    }
+
+    @BeforeAll
+    public static void startDocker() throws NotFoundException, IOException, InterruptedException {
+        
+        prop = TestUtil.loadProperties();
+
+        scenario = new DockerScenario();
+        
+      scenario.add("mongo", "mongo:4.4.0", 
+          "env:MONGO_INITDB_ROOT_USERNAME=mongoadmin",
+          "env:MONGO_INITDB_ROOT_PASSWORD=nein"
+      );
+
+        scenario.add(new Karaf("karaf", prop.getString("docker.mhus-apache-karaf.version"), 
+                "debug", 
+                "link:mongo:mongoserver"
+                ));
+        
+        scenario.destroyPrefix();
+        scenario.start();
+        
+        MThread.sleep(1000);
+
+        // jms
+        scenario.waitForLogEntry("mongo", "Waiting for connections", 0);
+        
+        // karaf
+        scenario.waitForLogEntry("karaf", "@karaf()>", 0);
+        
+        try (LogStream stream = new LogStream(scenario, "karaf")) {
+            stream.setFilter(new AnsiLogFilter());
+            scenario.attach(stream, 
+                "feature:repo-add mvn:org.apache.shiro/shiro-features/"+prop.getString("shiro.version")+"/xml/features\n" + 
+                "feature:repo-add mvn:de.mhus.osgi/mhus-features/"+prop.getString("mhus-parent.version")+"/xml/features\n" +
+                "feature:install mhu-mongo mhu-dev\n" +
+                "a=HGDFhjasdhj\n" );
+        
+            scenario.waitForLogEntry(stream, "Done.");
+        }
+
+        try (LogStream stream = new LogStream(scenario, "karaf")) {
+            stream.setFilter(new AnsiLogFilter());
+            stream.setCapture(true);
+            scenario.attach(stream, 
+                    "list\n" +
+                    "a=HGDFhjasdhx\n" );
+            scenario.waitForLogEntry(stream, "HGDFhjasdhx");
+            
+            String out = stream.getCaptured();
+            
+            int pos1 = out.indexOf("List Threshold");
+            assertTrue(pos1 > 0);
+            int pos2 = out.indexOf("@karaf()", pos1);
+            assertTrue(pos2 > 0);
+            out = out.substring(pos1, pos2);
+            
+            assertTrue(out.contains("lib-annotations"));
+            assertTrue(out.contains("lib-core"));
+            assertTrue(out.contains("lib-j2ee"));
+            assertTrue(out.contains("karaf-commands"));
+            assertTrue(out.contains("osgi-api"));
+            assertTrue(out.contains("osgi-services"));
+            assertTrue(out.contains("karaf-dev"));
+            
+        }
+        
+        try (LogStream stream = new LogStream(scenario, "karaf")) {
+            stream.setCapture(true);
+            stream.setFilter(new AnsiLogFilter());
+            scenario.attach(stream, 
+                    "dev-res -y cp default\n" +
+                    "dev-res -y cp examples-mongo\n" +
+                    "a=kjshkjfhjkIUYJGHJK\n" );
+
+            scenario.waitForLogEntry(stream, "kjshkjfhjkIUYJGHJK");
+            MThread.sleep(5000); // a long time - wait for configuration manager
+
+            String out = stream.getCaptured();
+
+//            assertTrue(out.contains("[doConfigure]"));
+            assertTrue(out.contains("[KarafCfgManager::Register PID][de.mhus.osgi.api.services.PersistentWatch]"));
+        }
+        
+        try (LogStream stream = new LogStream(scenario, "karaf")) {
+            scenario.attach(stream, 
+                    "access restart\n" +
+                    "a=HJGPODGHHKJNBHJGJHHJVU\n" );
+
+            scenario.waitForLogEntry(stream, "HJGPODGHHKJNBHJGJHHJVU");
+        }
+        
+    }
+    
+    @AfterAll
+    public static void stopDocker() {
+        // scenario.destroy();
+    }
+
+    
+}
