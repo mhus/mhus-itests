4a5c4e9925c298c54c0e42833f24589a1dff52a2
Mike Hummel <mike.hummel@billingsolutions.de>
2020-11-24 17:16:08 +0100

    Update KarafToolsTest.java

--- test-cases/src/test/java/de/mhus/lib/itest/cases/KarafToolsTest.java
+++ test-cases/src/test/java/de/mhus/lib/itest/cases/KarafToolsTest.java
@@ -481,6 +481,7 @@ public class KarafToolsTest extends TestCase {
             
             scenario.attach(stream, 
                     "dev-res -y cp default\n" +
+                    "dev-res -y cp disable-debug-log\n" +
                     "a=kjshkjfhjkIUYJGHJK\n" );
 
             scenario.waitForLogEntry(stream, "kjshkjfhjkIUYJGHJK");
